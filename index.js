846b15774091ed30a57668e308e4a51c0e2487f3
Cathrin Whitmer <catwhitmer@gmail.com>
2020-05-30 21:18:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 class Formatter {
-  //add static methods here
+ static capitalize(string) {
+    let phrase = string;
+    return phrase.charAt(0).toUpperCase() + phrase.slice(1);
+  }
+
+  static sanitize(string) {
+    let phrase = string;
+    return phrase.replace(/[^A-Za-z0-9-'\s]+/g, '');
+  }ds here
 }
