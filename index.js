62ae82bbb22477f16f1a6f35587df0a08edd4638
Cathrin Whitmer <catwhitmer@gmail.com>
2020-05-30 21:19:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 class Formatter {
- static capitalize(string) {
-    let phrase = string;
-    return phrase.charAt(0).toUpperCase() + phrase.slice(1);
+  static capitalize(string) {
+    let sentence = string;
+    return sentence.charAt(0).toUpperCase() + sentence.slice(1);
   }
 
   static sanitize(string) {
-    let phrase = string;
-    return phrase.replace(/[^A-Za-z0-9-'\s]+/g, '');
-  }ds here
-}
+    let sentence = string;
+    return sentence.replace(/[^A-Za-z0-9-'\s]+/g, '');
+  }
+
+static titleize(string) {
+    let sentence = string;
+    let words = string.split(" ");
+    let postArray = [];
+    let posts = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+    for (let i = 0; i < words.length; i++) {
+      if (i === 0) {
+        postArray.push(this.capitalize(words[i]));
+      } else if (posts.includes(words[i])) {
+        postArray.push(words[i])
+      } else {
+        postArray.push(this.capitalize(words[i]))
+      }
+    }
+    return postArray.join(" ");
+  }
